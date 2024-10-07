6ac316284995a7919d13be84ba60b98482332df4
ayush-parida <ayushparida999@gmail.com>
2024-10-08 03:32:22 +0530

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="next-gen-db-sdk",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["requests"],
+    description="Python SDK to interact with next-gen-db",
+    author="Ayush Parida",
+    author_email="ayushparida999@gmail.com",
+    url="https://github.com/ayush-parida/next-gen-db-sdk.git",
+)
+
